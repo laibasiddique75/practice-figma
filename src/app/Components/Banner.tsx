52cf353306323d9504400c92dfda542dcdd313c8
Laiba Siddique Qureshi <157726748+laibasiddique75@users.noreply.github.com>
2024-12-05 19:55:10 +0500

    added

--- src/app/Components/Banner.tsx
+++ src/app/Components/Banner.tsx
@@ -8,7 +8,7 @@ const Banner = () => {
       <div className="container col-xxl-8 px-4 py-5">
 <div className="row flex-lg-row-reverse align-items-center g-5 py-5">
  <div className="col-10 col-sm-8 col-lg-6" >
-<Image src="/banner.png" alt="banner" width={900} height={10000} layout="intrinsic" />
+<Image src="/banner.png" alt="banner" width={900} height={1000}  />
  </div>
 
   <div className="col-lg-6">
